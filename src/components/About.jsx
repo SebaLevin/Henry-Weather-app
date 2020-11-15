64fc880b0c977539c9ccc0429bdabf9d6b0f4ba4
unknown <sebalevin@hotmail.com.ar>
2020-11-15 20:31:42 -0300

    First push

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+//import { Link } from 'react-router-dom';
+
+function About(){
+    return(
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus exercitationem incidunt, eveniet inventore hic unde voluptates sint facilis atque itaque deserunt? Facere quas officia praesentium dolor exercitationem illo neque iure!</p>
+    );
+};
+
+export default About;
